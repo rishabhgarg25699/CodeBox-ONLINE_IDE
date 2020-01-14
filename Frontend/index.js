24a76092ce622eb16007c251feb476c0f9fa27e5
rishabhgarg25699 <rishabhgarg25699@gmail.com>
2020-01-14 21:36:55 +0530

    Changed

--- Frontend/index.js
+++ Frontend/index.js
@@ -3,14 +3,17 @@ editor.setTheme("ace/theme/twilight");
 editor.session.setMode("ace/mode/c_cpp");
 let inputbox = $('#input');
 let outputbox = $('#output');
+let wait = "Please wait, Your code is in process phase .......";
+
+
 
 $("#first_button").click(function () {
     let code = editor.getValue();
     let input1 = inputbox.val();
-
-    console.log(input);
+    outputbox.empty();
+    outputbox.append(wait);
+    // console.log(input);
     $.post('/todo/', { task: code, input: input1 }, function (data) {
-        // console.log("----------------index.js---aa gaya laut ke-------------------" + data);
         console.log(data);
         console.log(data.run_status.output);
         outputbox.empty();
