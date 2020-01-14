cc985f9e48a34a1e109d8b5aaad4638367f94706
rishabhgarg25699 <rishabhgarg25699@gmail.com>
2020-01-14 15:34:45 +0530

    Final

--- Frontend/index.js
+++ Frontend/index.js
@@ -2,15 +2,19 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
 editor.session.setMode("ace/mode/c_cpp");
 let inputbox = $('#input');
-
+let outputbox = $('#output');
 
 $("#first_button").click(function () {
     let code = editor.getValue();
     let input1 = inputbox.val();
+
     console.log(input);
     $.post('/todo/', { task: code, input: input1 }, function (data) {
-        console.log("----------------index.js---aa gaya laut ke-------------------" + data);
+        // console.log("----------------index.js---aa gaya laut ke-------------------" + data);
         console.log(data);
+        console.log(data.run_status.output);
+        outputbox.empty();
+        outputbox.append(data.run_status.output);
     })
 })
 
